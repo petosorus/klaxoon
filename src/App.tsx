17570dcfdb382832e4253fa89cd3f4cd2864e4db
petosorus <petosorus@hotmail.fr>
2020-07-30 17:33:51 +0200

    Basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
 import React from 'react';
+import {
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
+
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+        <Link to="/">root</Link>
+        <Link to="/newLink">new link</Link>
+      </nav>
+
+      <Switch>
+        <Route exact path="/">
+          Root
+        </Route>
+        <Route path="/newLink">
+          New link
+        </Route>
+      </Switch>
     </div>
   );
 }
